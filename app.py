f1cd6c42a8aa449d65fe86e62341fb5d8c055ac0
Sthitaprajna Mishra <sthitaprajna360@gmail.com>
2020-05-02 23:03:00 +0530

    added port

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
 from datetime import time
+import os
  
  
 app = Flask(__name__)
+
+port = int(os.getenv("VCAP_APP_PORT"))
  
  
 @app.route("/")
@@ -40,5 +43,5 @@ def chart():
     firstChartOne = firstChartOne, firstChartTwo = firstChartTwo)
  
  
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = port)
